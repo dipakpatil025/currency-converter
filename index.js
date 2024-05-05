77c40ecd0bcb3b0024064f807f8394619561cbf9
Dipak Patil <dipak.patil@noovosoft.com>
2024-05-05 15:42:55 +0530

    allow CORS

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const express = require('express');
-const CC = require('currency-converter-lt')
+const CC = require('currency-converter-lt');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// greeting home page
+// Set headers to allow CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // Allow specific HTTP methods
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+  next();
+});
+
+// Greeting home page
 app.get('/', (req, res) => {
   res.send('Welcome to Currency Converter API');
 });
 
-// currency converter api base 3 digit country code
-
+// Currency converter API
 app.get('/convert', async (req, res) => {
   const { from, to, amount } = req.query;
 
@@ -18,7 +25,7 @@ app.get('/convert', async (req, res) => {
   }
 
   try {
-    let currencyConverter = new CC({ from: from, to: to, amount: +amount })
+    let currencyConverter = new CC({ from: from, to: to, amount: +amount });
 
     const result = await currencyConverter.convert();
     res.json({ result, from, to, amount });
@@ -26,7 +33,6 @@ app.get('/convert', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
