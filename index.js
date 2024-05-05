76e6b7a782cc2922d3f71b9e5699fc567b7146da
Dipak Patil <dipak.patil@noovosoft.com>
2024-05-05 14:56:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const CC = require('currency-converter-lt')
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// currency converter api base 3 digit country code
+
+app.get('/convert', async (req, res) => {
+  const { from, to, amount } = req.query;
+
+  if (!from || !to || !amount) {
+    return res.status(400).json({ error: 'Missing parameters. Please provide "from", "to", and "amount".' });
+  }
+
+  try {
+    let currencyConverter = new CC({from:from, to:to, amount:+amount})
+
+    const result = await currencyConverter.convert();
+    res.json({ result , from, to, amount });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
